06114af2e52d0ddfd4685d54b95a35f1153a0bdc
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-02-09 15:14:13 +0300

    Сделал Юзер контрольлер с пост методом, для создания юзера

---
+++ src/main/java/ru/namazov/keme/controller/UserController.java
@@ -0,0 +1,18 @@
+package ru.namazov.keme.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin("*")
+public class UserController {
+
+
+    @PostMapping
+    public void createUser(UserDto userDto){
+
+    }
+}
