7f6d369efff67c8619ece729fb2039a4633bde8e
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-02-09 14:53:15 +0300

    Start

---
+++ src/main/java/ru/namazov/keme/KemeleoonApplication.java
@@ -0,0 +1,13 @@
+package ru.namazov.keme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KemeleoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KemeleoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/namazov/keme/KemeleoonApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.namazov.keme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KemeleoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
