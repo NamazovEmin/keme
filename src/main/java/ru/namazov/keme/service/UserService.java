866aa62fd19c76766d7d7f6e8b70aef70c1cda35
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-02-11 17:45:40 +0300

    Удалил все фасады репозиториев, все работает

--- src/main/java/ru/namazov/keme/service/UserService.java
+++ src/main/java/ru/namazov/keme/service/UserService.java
@@ -3,16 +3,16 @@ package ru.namazov.keme.service;
 import org.springframework.stereotype.Service;
 
 import ru.namazov.keme.entity.User;
-import ru.namazov.keme.repository.facade.UserFacadeRepository;
+import ru.namazov.keme.repository.UserRepository;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Service
 public class UserService {
-    private final UserFacadeRepository userFacadeRepository;
+    private final UserRepository userRepository;
 
     public User create(User user) {
-        return userFacadeRepository.save(user);
+        return userRepository.save(user);
     }
 }
