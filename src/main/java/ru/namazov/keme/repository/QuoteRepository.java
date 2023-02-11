388f3d065fbc9e0ddda01a37c7c8d4ae0021b904
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-02-11 17:38:53 +0300

    удалил тесты

--- src/main/java/ru/namazov/keme/repository/QuoteRepository.java
+++ src/main/java/ru/namazov/keme/repository/QuoteRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.namazov.keme.entity.Quote;
 
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
-    Quote findAllBy
 }
