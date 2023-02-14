6d1fab0d5da36c3a9056a28f9931435dd07e744b
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-02-14 17:13:09 +0300

    Добавил Секурити и Инкодинг

--- src/main/java/ru/namazov/keme/converter/UserDtoConverterImpl.java
+++ src/main/java/ru/namazov/keme/converter/UserDtoConverterImpl.java
@@ -1,19 +1,26 @@
 package ru.namazov.keme.converter;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import ru.namazov.keme.dto.UserDto;
 import ru.namazov.keme.dto.UserNewDto;
 import ru.namazov.keme.entity.User;
 
+import lombok.AllArgsConstructor;
+
 @Component
+@AllArgsConstructor
 public class UserDtoConverterImpl implements UserDtoConverter {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User toEntity(UserNewDto userNewDto) {
+        String hashedPassword = passwordEncoder.encode(userNewDto.getPassword());
         return new User(
                 userNewDto.getName(),
-                userNewDto.getPassword(),
+                hashedPassword,
                 userNewDto.getEmail()
         );
     }
